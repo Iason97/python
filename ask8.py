3c31bfe1dd1f18f0e3c8921e7ffc3134fb1d0d05
Iason97 <iasonas.vasiliadis@gmail.com>
2017-11-30 13:18:00 +0200

    Update ask8.py

--- ask8.py
+++ ask8.py
@@ -4,10 +4,10 @@ import tweepy
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
 
-consumer_key = "LWdAUcGIXb8wph2UdqHM8l9qu"
-consumer_secret ="8BpFG7a7vRkaqevtVtMgLbz6RPlc6NkCqgkc5YyKuSmy0eid4n"
-access_token = "3473182102-CuqzswAzY6YMRbXbqBd0JiXkaGHnwtZhkGDbdao"
-access_secret = "U4zggimICqJ10q1V6NyoHEtTF7adlnpconAQ5C1JdBlJi"
+consumer_key = ""
+consumer_secret =""
+access_token = ""
+access_secret = ""
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 api = tweepy.API(auth)
